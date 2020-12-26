cb7109652463451f87210e4e651980b91b086411
Andre Peixoto <peixoto.amp@gmail.com>
2020-12-26 00:47:59 -0300

    Sets date functions for creating appointments

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,9 +1,17 @@
 import { Router } from 'express';
 import { v4 as uuid } from 'uuid';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const appointmentsRouter = Router();
 
-const appointments = [];
+// Typescript. Using interface to define a type of an object (not a simple info)
+interface Appointments {
+  id: string;
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointments[] = [];
 
 // 'http://localhost:3330/appointments' is the root address of this route
 // as pointed in the index.ts file as "routes.use('/appointments', appointmentsRouter);"
@@ -11,14 +19,22 @@ const appointments = [];
 appointmentsRouter.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  ); // .find method returns a boolean
+
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({ message: 'This time is already taken' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
-
   return response.json(appointment);
 });
 
