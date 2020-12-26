d2f1a419c7ceda2bacae98fd0acf242bb7bbb277
Andre Peixoto <peixoto.amp@gmail.com>
2020-12-26 02:36:18 -0300

    Creates AppointmentsRepository & refactors Route

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,30 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  constructor() {
+    this.appointments = [];
+  }
+
+  public findByDate(date: Date): Appointment | null {
+    const findAppointment = this.appointments.find(appointment =>
+      isEqual(date, appointment.date),
+    );
+
+    return findAppointment || null;
+  }
+
+  // in Typescript it is recommended to declare what's the expected return for a method
+  // in the case below the expected return is: Appointment
+  public create(provider: string, date: Date): Appointment {
+    const appointment = new Appointment(provider, date);
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
