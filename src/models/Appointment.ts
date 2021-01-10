45e93a21a7267db8eddd79a148b7d3d1a8c43bdd
Andre Peixoto <peixoto.amp@gmail.com>
2021-01-10 11:02:39 -0300

    Creates relationship Appointment <-> User models

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -4,15 +4,25 @@ import {
   PrimaryGeneratedColumn,
   CreateDateColumn,
   UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
 } from 'typeorm';
+
+import User from './User';
 
 @Entity('appointments')
 class Appointment {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @Column()
-  provider: string;
+  @Column('uuid')
+  provider_id: string;
+
+  // This is a relationship between the 'provider_id' column (Appointment) with the 'id' column (User)
+  // of type ManyToOne because many appointments can be related to one user.
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'provider_id' })
+  provider: User;
 
   @Column('timestamp with time zone')
   date: Date;
