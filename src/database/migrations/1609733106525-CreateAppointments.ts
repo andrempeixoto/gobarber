b4c9de67b9a3ebf653ff3704f4434f949f87ccef
Andre Peixoto <peixoto.amp@gmail.com>
2021-01-10 08:25:46 -0300

    Creates new migration for Users -> Appointments

--- src/database/migrations/1609733106525-CreateAppointments.ts
+++ src/database/migrations/1609733106525-CreateAppointments.ts
@@ -9,7 +9,7 @@ export default class CreateAppointments1609733106525
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
--- src/database/migrations/1610202830277-CreateUsers.ts
+++ src/database/migrations/1610202830277-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1610202830277 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
