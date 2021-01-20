46e12b573d981d5075fff5b0cf28a4485344840e
Andre Peixoto <peixoto.amp@gmail.com>
2021-01-19 23:58:22 -0300

    Creates UpdateUserAvatarService

---
+++ src/services/UpdateUserAvatarService.ts
@@ -0,0 +1,10 @@
+interface Resquest {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpdateUserAvatarService {
+  public async execute({ user_id, avatarFilename }: Request): Promise<void> {}
+}
+
+export default UpdateUserAvatarService;
